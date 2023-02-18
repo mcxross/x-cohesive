6fb691eb164d01c2d9a602e5fe5fe2379719277a
@a <28899947+astinz@users.noreply.github.com>
2023-02-18 14:42:42 +0300

    add build, improve README...

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # cohesive-cohesive
-Cohesive Secondary Plugin Template. It includes all necessary files to create a secondary plugin for Cohesive.
+Cohesive Secondary Plugin Template. It includes all necessary files to create a Secondary Plugin for Cohesive.
+
+## What is a Secondary Plugin?
+Are you looking for a development environment for a particular Chain? If so, you might want to consider using a Secondary Plugin. These plugins are built on top of Cohesive and are tailored to work seamlessly with a specific Chain.
+
+## How to use
+1. Hit the `Use this template` button on the top right of this page
+2. Run `gradlew build` in the root directory of your project to build the plugin. A zip file will be created in the `build/plugin` directory after the build is complete
+3. You can drag and drop the plugin into a Cohesive instance to install it locally or submit it to the Cohesive Plugin Repository by following the instructions on the [Cohesive Plugin Repository](https://github.com/mcxross/cohesives) page
--- build.gradle.kts
+++ build.gradle.kts
@@ -12,12 +12,61 @@ repositories {
   mavenLocal()
 }
 
+val pluginId by extra { "com.mcxross.xcohesive" }
+val pluginDescription by extra { "Cohesive secondary plugin template" }
+val pluginRequires by extra { "2021.2" }
+val pluginClass by extra { "com.mcxross.xcohesive.CohesivePlugin" }
+val pluginProvider by extra { "McXross" }
+val pluginDependencies by extra { "" }
+val pluginLicense by extra { "Apache 2.0" }
+val pluginVersion by extra { "0.1.0" }
+
+val project = this
+
+tasks.register<Jar>("plugin") {
+  archiveBaseName.set(pluginId)
+
+  into("classes") { with(tasks.named<Jar>("jar").get()) }
+
+  dependsOn(configurations.runtimeClasspath)
+  into("lib") {
+    from({ configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") } })
+  }
+  archiveExtension.set("zip")
+}
+
+tasks.register<Copy>("assemblePlugin") {
+  from(project.tasks.named("plugin"))
+  into(file("$projectDir/build/plugin"))
+}
+
+tasks.named<Jar>("jar") {
+  manifest {
+    attributes["Plugin-Version"] = archiveVersion
+    attributes["Plugin-Id"] = pluginId
+    attributes["Plugin-Description"] = pluginDescription
+    attributes["Plugin-Class"] = pluginClass
+    attributes["Plugin-Dependencies"] = pluginDependencies
+    attributes["Plugin-Requires"] = pluginRequires
+    attributes["Plugin-Provider"] = pluginProvider
+    attributes["Plugin-License"] = pluginLicense
+  }
+}
+
+tasks.named("build") { dependsOn(tasks.named("plugin")) }
+
+tasks.register<Copy>("assemblePlugins") {
+  dependsOn(tasks.named("assemblePlugin"))
+}
+
+tasks { "build" { dependsOn(named("assemblePlugins")) } }
+
 dependencies {
   dependencies {
     compileOnly(kotlin("stdlib"))
     compileOnly("com.mcxross.cohesive:cohesive-desktop:0.1.0")
     compileOnly("com.mcxross.cohesive:cohesive-csp:0.1.0")
-    ksp("com.mcxross.cohesive.csp:cohesive-csp:0.1.0")
+    ksp("com.mcxross.cohesive:cohesive-csp:0.1.0")
   }
   testImplementation(kotlin("test"))
 }
